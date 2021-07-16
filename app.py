0e1ed8dae2a0744b8e06e09a6394e943aaa27888
Jerry Hsu <Jerry>
2021-07-16 11:50:33 -0400

    test 7, setup for heroku

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 import os
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'postgresql:///ball-dont-lie').replace("://", "ql://", 1))
+    'DATABASE_URL', 'postgresql:///ball-dont-lie').replace("://", "ql://", 1)\
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
